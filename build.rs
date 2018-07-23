379f170ed14b593bd0a8adc2be622d295ffd79d6
Matthias Wimmer <m@tthias.eu>
2018-07-24 00:31:46 +0200

    Generate configure scripts before building.

--- build.rs
+++ build.rs
@@ -6,12 +6,20 @@ use std::fs::File;
 use std::process::{Command, Stdio};
 
 fn main() -> std::io::Result<()> {
+    let axl_include_path = format!("-I{}/include/axl",
+                                   env::var("OUT_DIR").unwrap());
     let pkg_config_path = format!("{}/lib/pkgconfig",
                                   env::var("OUT_DIR").unwrap());
 
+    Command::new("./autogen.sh")
+        .arg("--help")
+        .current_dir("libaxl")
+        .status()
+        .expect("Failed to generate libaxl/configure");
     Config::new("libaxl")
         .disable("-axl-knife", None)
         .disable("-py-axl", None)
+        .env("CFLAGS", axl_include_path)
         .build();
 
     Command::new("patch")
@@ -21,7 +29,11 @@ fn main() -> std::io::Result<()> {
         .stdin(Stdio::from(File::open("vortex_openssl11.patch")?))
         .status()
         .expect("Failed to patch vortex");
-
+    Command::new("./autogen.sh")
+        .arg("--help")
+        .current_dir("libvortex")
+        .status()
+        .expect("Failed to generate libvortex/configure");
     let dst = Config::new("libvortex")
         .disable("-py-vortex", None)
         .env("PKG_CONFIG", "pkg-config --static")
