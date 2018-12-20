0c45a2c4ea21e24c2964203713df2026d5249305
RobertHanyecz <rh@wertschuetz.de>
2018-12-20 11:36:40 +0100

    changed wrong function name used fot ssl 1.0

--- libvortex/tls/vortex_tls.c
+++ libvortex/tls/vortex_tls.c
@@ -734,7 +734,7 @@ int      vortex_tls_invoke_tls_activation (VortexConnection * connection)
 #elif defined(VORTEX_HAVE_TLSv11_ENABLED) && OPENSSL_VERSION_NUMBER < 0x10100000L
 		ssl_ctx  = SSL_CTX_new (TLSv1_1_client_method ());
 #elif defined(VORTEX_HAVE_TLSv10_ENABLED) && OPENSSL_VERSION_NUMBER < 0x10100000L
-		ssl_ctx  = SSL_CTX_new (TLSv1_0_client_method ());
+		ssl_ctx  = SSL_CTX_new (TLSv1_client_method ());
 #elif defined(VORTEX_HAVE_SSLv3_ENABLED) && OPENSSL_VERSION_NUMBER < 0x10100000L
 		ssl_ctx  = SSL_CTX_new (SSLv3_client_method ());
 #else
@@ -2437,7 +2437,7 @@ char* vortex_tls_get_ssl_digest (const char * path, VortexDigestMethod   method)
 #elif defined(VORTEX_HAVE_TLSv11_ENABLED) && OPENSSL_VERSION_NUMBER < 0x10100000L
 	sslctx  = SSL_CTX_new (TLSv1_1_server_method ());
 #elif defined(VORTEX_HAVE_TLSv10_ENABLED) && OPENSSL_VERSION_NUMBER < 0x10100000L
-	sslctx  = SSL_CTX_new (TLSv1_0_server_method ());
+	sslctx  = SSL_CTX_new (TLSv1_server_method ());
 #elif defined(VORTEX_HAVE_SSLv3_ENABLED) && OPENSSL_VERSION_NUMBER < 0x10100000L
 	sslctx  = SSL_CTX_new (SSLv3_server_method ());
 #else
